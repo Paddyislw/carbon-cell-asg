b7482497870be6aab1a05d678bee13acdf2b40f4
Paddyislw <pradyum0701@gmail.com>
2024-03-31 22:58:06 +0530

    added asset page

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import { useState } from "react";
 import NavigationBar from "./components/NavigationBar";
 import HomePage from "./pages/HomePage";
 import ConnectWalletPage from "./pages/ConnectWalletPage";
+import AssetPage from "./pages/AssetPage";
 
 const App = () => {
   const [currentTab, setCurrentTab] = useState("home");
   return (
     <div className="flex">
       <NavigationBar currentTab={currentTab} setCurrentTab={setCurrentTab} />
-      {currentTab ==='home' && <HomePage />}
-      {currentTab ==='connectWallet' && <ConnectWalletPage />}
+      {currentTab === "home" && <HomePage />}
+      {currentTab === "connectWallet" && <ConnectWalletPage />}
+      {currentTab === "assets" && <AssetPage />}
     </div>
   );
 };
---
+++ src/pages/AssetPage.tsx
@@ -0,0 +1,9 @@
+const AssetPage = () => {
+  return (
+    <div className="w-full bg-[#0b0b0b]">
+      
+    </div>
+  )
+}
+
+export default AssetPage
